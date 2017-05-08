a4c24bf75f307d5124030f56c347ce9837cce5c2
Martini09 <yeyuan9393@gmail.com>
2017-05-08 19:37:10 -0400

    matrix element sum

--- src/gpu_op.cu
+++ src/gpu_op.cu
@@ -167,9 +167,32 @@ int DLGpuMatrixElementwiseAdd(const DLArrayHandle matA,
   return 0;
 }
 
+
+__global__ void matrix_elementwise_add_by_const_kernel(const float *input_data,
+                                                       float *output_data,
+                                                       float val,
+                                                       index_t n) {
+  index_t idx = blockDim.x * blockIdx.x + threadIdx.x;
+  if (idx < n) 
+    output_data[idx] = input_data[idx] * val;
+}
+
+
 int DLGpuMatrixElementwiseAddByConst(const DLArrayHandle input, float val,
                                      DLArrayHandle output) {
   /* TODO: Your code here */
+  index_t n = 1;
+  for (int i = 0; i < output->ndim; i++)
+    n *= output->shape[i];
+
+  const float *input_data = (const float *) input->data;
+  float *output_data = (float *) output->data;
+  int thread_per_block = 512;
+  int n_blocks = (n + thread_per_block - 1) / thread_per_block;
+  matrix_elementwise_add_by_const_kernel<<<n_blocks, thread_per_block>>>(input_data, 
+                                                                         output_data,
+                                                                         val,
+                                                                         n); 
   return 0;
 }
 
